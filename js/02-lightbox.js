1001ac2e18d575c4d8204acaa3a4dc955c18c261
Marina Ishchenko <marina.s.ishchenko@gmail.com>
2022-07-17 17:39:13 -0700

    task02 - done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,15 +5,25 @@ const galleryEl = document.querySelector(".gallery")
 console.log(galleryItems);
 
 function createGallary (galleryItems){
-    return galleryItems.reduce((acc, item) => acc + `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    return galleryItems.reduce((acc, item) => acc + 
+    `<a class="gallery__item" href="${item.original}">
       <img
         class="gallery__image"
         src="${item.preview}"
-        data-source="${item.original}"
         alt="${item.description}"
       />
     </a>
-  </div>`, ""
+  `, ""
     )
 }
+
+const resultOfCreateGallery = createGallary (galleryItems)
+galleryEl.insertAdjacentHTML("beforeend", resultOfCreateGallery)
+
+let gallery = new SimpleLightbox('.gallery a', 
+{captionsData: "alt", 
+captionDelay: 250,
+captionPosition: "bottom"});
+// gallery.on('show.simplelightbox', function () {
+//     // Do something…
+// })
